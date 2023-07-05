50921d9fee99e62129a97fdcb27f41a148faa1df
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2023-07-05 10:17:11 +0200

    Adding process variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),
